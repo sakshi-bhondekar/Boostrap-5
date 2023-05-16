fb34790bbe1500487c3114cf3b4cd71609e8bbb5
Sakshi <sakshibhondekar07@gmail.com>
2023-05-17 00:04:11 +0530

    Form Input Handling

--- bs5/register.php
+++ bs5/register.php
@@ -2,6 +2,20 @@
 <?php
 include_once ("includes/head.php");
 include_once ("includes/navbar.php");
+
+
+if(ISSET($_GET["user_name"]) && ISSET($_GET["user_email"]) && ISSET($_GET["user_password"]) && ISSET($_GET["cnf_user_password"])){
+$user_name = $_GET["user_name"];
+$user_email = $_GET["user_email"];
+$user_password = $_GET["user_password"];
+$cnf_user_password = $_GET["cnf_user_password"];
+
+  if ($user_password != $cnf_user_password){
+    echo "Please input the same password";
+  }
+  
+}
+
 ?>
 <!-- Register Page :: Start -->
 <section class="vh-80" style="background-color: #eee;">
@@ -15,12 +29,13 @@ include_once ("includes/navbar.php");
 
                 <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
 
-                 <form action="profile.php" class="mx-1 mx-md-4">
+                 <form class="mx-1 mx-md-4">
+                 <!-- <form action="register.php" class="mx-1 mx-md-4"> -->
 
                   <div class="d-flex flex-row align-items-center mb-4">
                     <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
-                      <input type="text" id="form3Example1c" class="form-control" />
+                      <input type="text" id="form3Example1c" class="form-control" name="user_name"/>
                       <label class="form-label" for="form3Example1c">Your Name</label>
                     </div>
                   </div>
@@ -28,7 +43,7 @@ include_once ("includes/navbar.php");
                   <div class="d-flex flex-row align-items-center mb-4">
                     <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
-                      <input type="email" id="form3Example3c" class="form-control" />
+                      <input type="email" id="form3Example3c" class="form-control" name="user_email"/>
                       <label class="form-label" for="form3Example3c">Your Email</label>
                     </div>
                   </div>
@@ -36,7 +51,7 @@ include_once ("includes/navbar.php");
                   <div class="d-flex flex-row align-items-center mb-4">
                     <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
-                      <input type="password" id="form3Example4c" class="form-control" />
+                      <input type="password" id="form3Example4c" class="form-control" name="user_password"/>
                       <label class="form-label" for="form3Example4c">Password</label>
                     </div>
                   </div>
@@ -44,17 +59,19 @@ include_once ("includes/navbar.php");
                   <div class="d-flex flex-row align-items-center mb-4">
                     <i class="fas fa-key fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
-                      <input type="password" id="form3Example4cd" class="form-control" />
+                      <input type="password" id="form3Example4cd" class="form-control" name="cnf_user_password"/>
                       <label class="form-label" for="form3Example4cd">Repeat your password</label>
                     </div>
                   </div>
 
+                  <!--
                   <div class="form-check d-flex justify-content-center mb-5">
-                    <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3c" />
+                    <input class="form-check-input me-2" type="checkbox" id="form2Example3c"/>
                     <label class="form-check-label " for="form2Example3">
                       I agree all statements in <a class="active" href="terms-condition.php">Terms of service</a>
                     </label>
                   </div>
+                  -->
 
                   <div class="d-flex justify-content-center mx-4 my-1 mb-3 mb-lg-4">
                     <button type="submit" class="btn btn-primary btn-lg">Register</button>
